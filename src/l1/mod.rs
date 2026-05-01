9f18056713154647a14992a5c815bcb17eb3a07e
deval <devald@stanford.edu>
2026-04-30 23:49:29 -0700

    re-exporting l1 funcs to parent module

--- src/l1/mod.rs
+++ src/l1/mod.rs
@@ -1,5 +1,5 @@
-pub mod axpy;
 pub mod dot; 
+pub mod axpy;
 pub mod asum; 
 pub mod scal; 
 pub mod nrm2; 
@@ -7,3 +7,11 @@ pub mod copy;
 pub mod swap; 
 pub mod iamax;
 
+pub use dot::dot; 
+pub use axpy::axpy; 
+pub use asum::asum; 
+pub use scal::scal; 
+pub use nrm2::nrm2; 
+pub use copy::copy; 
+pub use swap::swap; 
+pub use iamax::iamax; 
