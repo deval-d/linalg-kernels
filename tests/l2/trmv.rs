57392bd2ace5347eb174a00bd0869a3d0254b4d4
deval <devald@stanford.edu>
2026-05-08 00:01:11 -0700

    trmv upper no transpose tests

--- tests/l2/trmv.rs
+++ tests/l2/trmv.rs
@@ -41,10 +41,46 @@ fn dtrmv_ln(case: u64) {
     assert_close(&xbuf, &xbuf_blas); 
 }
 
+fn dtrmv_un(case: u64) { 
+    let n = 512; 
+
+    let mut rng = test_rng(case); 
+    let mut xbuf: Vec<f64> = make_vec_random(n, &mut rng); 
+    let abuf: Vec<f64> = make_vec_random(n * n, &mut rng);
+    let mut xbuf_blas = xbuf.clone(); 
+
+    let x = VecMut::new(&mut xbuf); 
+    let a = MatRef::new(&abuf, (n, n));
+
+    trmv(
+        Triangular::Upper, 
+        Transpose::NoTranspose,
+        a, 
+        x,
+    );
+
+    unsafe { 
+        cblas_dtrmv( 
+            cblas_sys::CBLAS_LAYOUT::CblasColMajor,
+            cblas_sys::CBLAS_UPLO::CblasUpper, 
+            cblas_sys::CBLAS_TRANSPOSE::CblasNoTrans, 
+            cblas_sys::CBLAS_DIAG::CblasNonUnit, 
+            n as i32, 
+            abuf.as_ptr(), 
+            n as i32, 
+            xbuf_blas.as_mut_ptr(), 
+            1 as i32,
+        )
+    } 
+
+    assert_close(&xbuf, &xbuf_blas); 
+}
+
 #[test] 
 fn main() { 
     for case in 0..ITERATIONS { 
        dtrmv_ln(case); 
+       dtrmv_un(case); 
     }
 }
 
