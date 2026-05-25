9bb3940197715fc990d13c905ef11d6c9a515e06
deval <devald@stanford.edu>
2026-05-25 00:38:41 -0700

    ignore

--- src/bin/sdot_asm.rs
+++ src/bin/sdot_asm.rs
@@ -1,4 +1,5 @@
 use lak::l1::dot::dot;
+use lak::traits::Fma;
 use lak::types::VecRef;
 
 #[inline(never)]
@@ -13,7 +14,29 @@ fn main() {
     let x = vec![1.0f32; 1024];
     let y = vec![2.0f32; 1024];
 
-    let out = sdot(&x, &y);
+    let out = dot_naive(&x, &y);
+    let out2 = sdot(&x, &y); 
 
     std::hint::black_box(out);
+    std::hint::black_box(out2);
 }
+
+#[inline(never)]
+fn dot_naive( 
+    x: &[f32], 
+    y: &[f32], 
+) -> f32 {
+    let x = VecRef::new(x); 
+    let y = VecRef::new(y); 
+
+    let xdata = x.as_slice(); 
+    let ydata = y.as_slice(); 
+
+    let mut sum = 0.0; 
+    for (&xi, &yi) in xdata.iter().zip(ydata.iter()) { 
+        sum = xi.fma(yi, sum); 
+    }
+
+    sum
+}
+
