fc479a45ab68bb2a9db4ecd26cf642515841d733
deval <devald@stanford.edu>
2026-05-24 20:06:41 -0700

    added well behaved, diagonally dominant for TRSV tests

--- src/helpers.rs
+++ src/helpers.rs
@@ -33,3 +33,30 @@ where
     buffer
 }
 
+#[allow(dead_code)]
+pub fn make_behaved_mat_dd_f64(
+    n: usize,
+    rng: &mut StdRng,
+) -> Vec<f64> {
+    assert!(n != 0, "n must be nonzero");
+
+    let mut a = vec![0.0f64; n * n];
+
+    for j in 0..n {
+        for i in j + 1..n {
+            a[i + j * n] = rng.gen_range(-1.0..1.0);
+        }
+    }
+
+    for i in 0..n {
+        let mut row_sum = 0.0;
+
+        for j in 0..i {
+            row_sum += a[i + j * n].abs();
+        }
+
+        a[i + i * n] = row_sum + 1.0;
+    }
+
+    a
+}
